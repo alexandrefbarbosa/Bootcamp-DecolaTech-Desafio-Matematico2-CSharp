5819ead513a72b572165dbc5e1bf608d152c7a88
Alexandre Barbosa <alexandreferreirabarbosa@gmail.com>
2021-09-21 04:09:44 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Somasimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração das variáveis
+            int A, B, soma;
+
+        
+            Console.Write("Informe o 1º número -> ");
+            
+            //Lê o valor digitado
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o 2º número -> ");
+            B = int.Parse(Console.ReadLine());
+
+            soma = A + B;     
+            
+            //Imprimi o resultado
+            Console.WriteLine("SOMA = " + soma);
+
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Bootcamp da DIO Decola Tech Desafios C Sharp :airplane:
+Este é um desafio matemático da Digital Innovation one para o bootcamp Decola Tech da Avanade que faz uma soma simples em C Sharp.
